d297c0520de00e512acc9ebee44cbd6d7431f28c
woj289 <woj289@gmail.com>
2017-11-07 09:49:36 +0100

    1.0
    Ver 1.0

---
+++ script.js
@@ -0,0 +1,28 @@
+
+$(document).ready(function(){
+
+  for (var i = 0; i < 3; i++) {
+      $('.dot').prepend('<div class="dot-skills"><img src="image/dot1.png" /></div>');
+    }
+
+  for (var i = 0; i < 9; i++) {
+      $('.dot').prepend('<div class="dot-skills"><img src="image/dot.png" /></div>');
+    }
+
+  for (var i = 0; i < 3; i++) {
+      $('.dot1').prepend('<div class="dot-skills"><img src="image/dot1.png" /></div>');
+    }
+
+  for (var i = 0; i < 9; i++) {
+      $('.dot1').prepend('<div class="dot-skills"><img src="image/dot.png" /></div>');
+    }
+
+  for (var i = 0; i < 3; i++) {
+      $('.dot2').prepend('<div class="dot-skills"><img src="image/dot1.png" /></div>');
+      }
+
+  for (var i = 0; i < 9; i++) {
+      $('.dot2').prepend('<div class="dot-skills"><img src="image/dot.png" /></div>');
+    }
+
+});
